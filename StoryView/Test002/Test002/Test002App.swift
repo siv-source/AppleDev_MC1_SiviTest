46133497e8820b0688214ab42ca0f7c84a86bbaf
leeve1247 <69492686+leeve1247@users.noreply.github.com>
2023-03-27 11:59:53 +0900

    StoryView
    스토리 뷰 올렸습니다리

---
+++ StoryView/Test002/Test002/Test002App.swift
@@ -0,0 +1,17 @@
+//
+//  Test002App.swift
+//  Test002
+//
+//  Created by Kihyun Roh on 2023/03/27.
+//
+
+import SwiftUI
+
+@main
+struct Test002App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
