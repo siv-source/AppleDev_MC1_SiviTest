222b73a98b14bfa7e41e7c30e522f6f95b96584f
Seokmin Jung <79754613+shine-jung@users.noreply.github.com>
2023-03-30 10:04:46 +0900

    feat: add .gitignore & splashView

---
+++ StoryView/Test003.swiftpm/SplashView.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        VStack {
+            Image("logo")
+                .resizable()
+                .frame(width: 150, height: 150)
+                .foregroundColor(.accentColor)
+                .offset(y:-1)
+            Text("나는 어떤 러너일까!?")
+                .offset(y: -8)
+            
+        }
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
